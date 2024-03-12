0bfcdaaf5188ab3367de9d1852f5f5a59d886116
Théo Brasseur <theobrasseurlille@hotmail.com>
2024-03-12 09:37:28 +0100

    fix menu

--- src/component/FooterMenu.jsx
+++ src/component/FooterMenu.jsx
@@ -6,8 +6,8 @@ const FooterMenu = () => {
       <ul>
         <li><a href="#presentation">PRESENTATION</a></li>
         <li><a href="#mon-parcours">MON PARCOURS</a></li>
-        <li><a href="#acces">ACCES</a></li>
-        <li><a href="#references">REFERENCES</a></li>
+        <li><a href="#acces" className='nt'>ACCES</a></li>
+        <li><a href="#references" className='nt'>REFERENCES</a></li>
         <li><a href="#contact">CONTACT</a></li>
       </ul>
     </div>
